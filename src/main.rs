de90cc2de91e92cc0fc20676a03d77c99e7f9553
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-20 11:32:06 +0300

    dinamik func
    how to use dinamik func with dinamik variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fmt::Display;
+
 fn main() {
-    
+
+}
+
+fn dinamikfunc(){
+    let string = String::from("Hello");
+    let num = 777;
+
+    print_value(num,string);
+}
+
+fn print_value<T: Display,S:Display>(value:T,text:S){
+    println!("{} {}",value,text);
 }
