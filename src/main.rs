a54c2cef653635b299cb8a83104d32167f068ce0
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-21 12:34:33 +0300

    end

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
+use std::collections::hash_map::Keys;
 use std::fmt::Display;
 
-fn main() {
-
+struct Data<T, K> {
+    d1: T,
+    d2: K,
 }
 
-fn dinamikfunc(){
+impl<T, K> Data<T, K> {
+    fn get_data_1(&self)->&T{
+        &self.d1
+    }
+    fn get_data_2(&self)->&K{
+        &self.d2
+    }
+}
+
+fn main() {
+    let data = Data {
+        d1: 2.32,
+        d2: "July".to_string(),
+    };
+    println!("Data 1:{}\nData 2:{}",data.get_data_1(),data.get_data_2());
+}
+
+fn dinamikfunc() {
     let string = String::from("Hello");
     let num = 777;
 
-    print_value(num,string);
+    print_value(num, string);
 }
 
-fn print_value<T: Display,S:Display>(value:T,text:S){
-    println!("{} {}",value,text);
+fn print_value<T: Display, S: Display>(value: T, text: S) {
+    println!("{} {}", value, text);
 }
